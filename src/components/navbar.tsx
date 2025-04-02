bfe7c026631068711d904c1a7cf0ea6e93f9c6c9
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2025-04-02 19:20:19 +0530

    test push

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,10 +5,28 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
 import {motion} from 'framer-motion'
 
 
-const Navbar = () => {
-  return (
-    <div>navbar</div>
-  )
-}
 
+
+const Navbar = () => {
+    const navlinks = [
+      { title: "About", path: "" },
+      { title: "Portfolio", path: "" }
+    ];
+  
+    return (
+      <div className="text-white/70 pt6">
+        <div className="md:flex items-center px-4 py-2 mx-auto max-w-[400px]">
+          <ul>
+            {navlinks.map((link, index) => (
+              <li key={index}>
+                <Link href={link.path}>
+                  <p>{link.title}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
 export default Navbar
