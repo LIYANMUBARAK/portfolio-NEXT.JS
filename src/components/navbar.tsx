bb90f37508e70414d2644393bb7631a4c3216c2b
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2025-04-02 18:39:11 +0530

    intial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Link from "next/link"
+import React,{useState} from "react"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+import {motion} from 'framer-motion'
+
+
+const Navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default Navbar
